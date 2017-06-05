5f397b2e98769464e1d01cde2616f3d71c6dcb00
Fernando De La Cruz Soto <fdelacruzsoto@gmail.com>
2017-06-05 17:38:08 -0500

    Nodejs
    Add nodes to test the app running

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-'use strict'
+var express     = require('express');
+var listen_port = process.env.PORT || 3000;
+var dev         = process.env.DEV;
+var app         = express();
+var morgan      = require('morgan');
 
-const app = angular.module('followersApp', [])
+app.use(morgan("dev"));
+app.use(express.static("./static"));
+app.listen(listen_port, function() {
+    console.log("All right ! I am alive at Port " + listen_port + ".");
+  });
